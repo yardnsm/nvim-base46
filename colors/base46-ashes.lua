35628dd2259daf2801b1321d77c11c5405e7a3d2
Yarden Sod-Moriah <yardnsm@gmail.com>
2024-06-12 10:52:41 +0300

    Added themes from the original base46 repo

---
+++ colors/base46-ashes.lua
@@ -0,0 +1,7 @@
+-- AUTO-GENERATED FILE. DO NOT MODIFIY.
+-- This theme was ported from https://github.com/NvChad/base46
+
+local status_ok, base46 = pcall(require, "nvim-base46")
+if status_ok then
+  base46.load({ theme = "ashes" })
+end
